1a870a43fc2a036ea694500f47f6293aab547cc5
hermanus rangga <hermanus.rangga@ti.ukdw.ac.id>
2025-06-16 03:04:20 +0700

    asd

--- Assets/script/FireController.cs
+++ Assets/script/FireController.cs
@@ -5,14 +5,29 @@ public class FireController : MonoBehaviour
     private bool isBeingExtinguished = false;
     private float extinguishSpeed = 1f;
     private Vector3 originalScale;
+    private ParticleSystem[] childParticleSystems;
 
-    void Start() { originalScale = transform.localScale; }
+    void Start()
+    {
+        originalScale = transform.localScale;
+        childParticleSystems = GetComponentsInChildren<ParticleSystem>();
+    }
 
     void Update()
     {
         if (isBeingExtinguished)
         {
+            // Gradually scale down the object
             transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, Time.deltaTime * extinguishSpeed);
+
+            // Gradually reduce emission rates of all child particle systems
+            foreach (var ps in childParticleSystems)
+            {
+                var emission = ps.emission;
+                emission.rateOverTime = Mathf.Lerp(emission.rateOverTime.constant, 0, Time.deltaTime * extinguishSpeed);
+            }
+
+            // Disable the object when fully extinguished
             if (transform.localScale.magnitude <= 0.05f)
             {
                 gameObject.SetActive(false);
@@ -21,5 +36,8 @@ public class FireController : MonoBehaviour
         }
     }
 
-    public void Extinguish() { isBeingExtinguished = true; }
+    public void Extinguish()
+    {
+        isBeingExtinguished = true;
+    }
 }
--- Assets/script/PemadamSc.cs
+++ Assets/script/PemadamSc.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class PemadamSc : MonoBehaviour
 {
+
     void OnParticleCollision(GameObject other)
+    {
+
+        Debug.Log("Particle collision with: " + other.name);
+    }
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Fire"))
         {
-            FireController fire = other.GetComponent<FireController>();
+            FireController fire = other.GetComponentInParent<FireController>();
             if (fire != null)
             {
                 fire.Extinguish();
             }
         }
-        Debug.Log(other.name);
     }
 }
